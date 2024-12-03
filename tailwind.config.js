56795bb2c20db821f004a6aa000dd99d9022ad8a
M. Zaky Pria Maulana <zakymaulana363@gmail.com>
2024-12-04 01:12:52 +0700

    portfolio | init project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
+	theme: {
+		container: {
+			center: true,
+			padding: '15px',
+		},
+
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1200px',
+		},
+
+		fontFamily: {
+			primary: 'var(--font-bebas)',
+			secondary: 'var(--font-inter)',
+		},
+
+		extend: {
+			colors: {
+				primary: '#10151f',
+				secondary: '#475467',
+				accent: {
+					DEFAULT: '#7edad2',
+					hover: '#79d3cc',
+				},
+			},
+			boxShadow: {
+				custom: '0px 14px 54px rgba(0,0,0,0.1',
+			},
+		},
+	},
+	plugins: [],
 };
